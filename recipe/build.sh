7fdadac78ceea32e76ab3a4ab453b0ba19de4796
Isuru Fernando <isuruf@gmail.com>
2020-11-12 19:45:03 -0600

    fixes for cross compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,25 @@ fi
 
 chmod +x configure
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+ (export CC=$CC_FOR_BUILD
+  export CFLAGS=""
+  export CXXFLAGS=""
+  export CPPFLAGS=""
+  export LDFLAGS="-L$BUILD_PREFIX/lib -Wl,-rpath,$BUILD_PREFIX/lib"
+  ./configure \
+        --prefix="$BUILD_PREFIX" \
+        --gmp-prefix="$BUILD_PREFIX" \
+        --cflags="$CFLAGS" \
+        --ldflags="$LDFLAGS" \
+        --cppflags="$CPPFLAGS" \
+        --cxxflags="$CXXFLAGS"
+ )
+ make tune
+ cp tune/tune $BUILD_PREFIX/bin/
+ make clean
+fi
+
 ./configure \
         --prefix="$PREFIX" \
         --gmp-prefix="$PREFIX" \
@@ -18,15 +37,14 @@ chmod +x configure
         --cppflags="$CPPFLAGS" \
         --cxxflags="$CXXFLAGS"
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  $BUILD_PREFIX/bin/tune > src/tuning.c
+  make
+else
   make tune
   tune/tune > src/tuning.c
   make
   make check
-else
-  CC=$CC_FOR_BUILD CFLAGS="" CXXFLAGS="" CPPFLAGS="" LDFLAGS="-L$BUILD_PREFIX/lib -Wl,-rpath,$BUILD_PREFIX/lib" make tune
-  tune/tune > src/tuning.c
-  make
 fi
 
 make libzn_poly${SHLIB_EXT}
